c2e03b955fe80ab3f2f50b38c4b08e259b352004
decole <decole2014@yandex.ru>
2022-09-30 23:01:30 +0300

    [UBS-39] Сделать фоновый процесс, который будет раз в минуту перебирать list и удалять просроченные данные

--- src/Application/Service/DeviceData/DeviceDataCacheService.php
+++ src/Application/Service/DeviceData/DeviceDataCacheService.php
@@ -28,17 +28,7 @@ final class DeviceDataCacheService
             'createdAt' => time(),
         ];
 
-        $this->cache->set(
-            key: CacheKeyListEnum::DEVICE_TOPICS_LIST,
-            value: $map,
-            tags: [CacheKeyListEnum::DEVICE_TOPICS_LIST],
-            lifetime: self::CACHE_LIMIT
-        );
-    }
-
-    public function getList(): array
-    {
-        return $this->cache->get(CacheKeyListEnum::DEVICE_TOPICS_LIST)[self::LIST_KEY] ?? [];
+        $this->setCache($map);
     }
 
     public function getPayloadByTopicList(array $topics): array
@@ -50,6 +40,24 @@ final class DeviceDataCacheService
         }
 
         return $result;
+    }
+
+    public function clearOldPayload(): void
+    {
+        $map = $this->getList();
+
+        foreach ($map as $cachedTopic => $payload) {
+            if ($this->isExpiredPayload($payload['createdAt'])) {
+                unset($map[$cachedTopic]);
+            }
+        }
+
+        $this->setCache($map);
+    }
+
+    private function getList(): array
+    {
+        return $this->cache->get(CacheKeyListEnum::DEVICE_TOPICS_LIST)[self::LIST_KEY] ?? [];
     }
 
     private function getTopicPayload(mixed $topic): ?string
@@ -65,9 +73,18 @@ final class DeviceDataCacheService
         return null;
     }
 
-    // todo сделать фоновый процесс, который будет раз в минуту перебирать list и удалять просроченные данные
-    public function isExpiredPayload(int $payloadTime): bool
+    private function isExpiredPayload(int $payloadTime): bool
     {
         return time() > $payloadTime + self::CACHE_LIMIT;
+    }
+
+    private function setCache(array $map): void
+    {
+        $this->cache->set(
+            key: CacheKeyListEnum::DEVICE_TOPICS_LIST,
+            value: $map,
+            tags: [CacheKeyListEnum::DEVICE_TOPICS_LIST],
+            lifetime: self::CACHE_LIMIT
+        );
     }
 }
