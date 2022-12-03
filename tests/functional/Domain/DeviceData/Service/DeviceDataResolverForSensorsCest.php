f90aad6144a3fa2a0cbe338517fca02c9c638851
decole <decole2014@yandex.ru>
2022-12-03 21:14:34 +0300

    [UBS-15] первые тесты по sensor

--- tests/functional/Domain/DeviceData/Service/DeviceDataResolverForSensorsCest.php
+++ tests/functional/Domain/DeviceData/Service/DeviceDataResolverForSensorsCest.php
@@ -7,12 +7,11 @@ use App\Domain\DeviceData\Service\DeviceDataCacheService;
 use App\Domain\DeviceData\Service\DeviceDataResolver;
 use App\Domain\DeviceData\Service\DeviceDataValidationService;
 use App\Domain\Payload\Entity\DevicePayload;
-use App\Domain\Sensor\Entity\TemperatureSensor;
 use App\Tests\_support\Step\FunctionalStep\Domain\DeviceData\Service\DeviceDataResolverStep;
-use App\Tests\ExampleCest;
 use Codeception\Example;
 use Codeception\Stub;
 use Codeception\Stub\Expected;
+use Psr\Cache\InvalidArgumentException;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 
 class DeviceDataResolverForSensorsCest
@@ -20,14 +19,17 @@ class DeviceDataResolverForSensorsCest
     /**
      * @var EntityInterface[]
      */
-    private array $list;
+    private ?array $list = null;
     private DeviceDataResolver $resolver;
     private DeviceDataValidationService $validateService;
     private DeviceDataCacheService $cacheService;
 
     public function _before(DeviceDataResolverStep $I)
     {
-        $this->list = $I->createAllTypeSensors($I);
+        if ($this->list === null) {
+            $this->list = $I->createAllTypeSensors($I);
+        }
+
         $this->resolver = $I->grabService(DeviceDataResolver::class);
         $this->validateService = $I->grabService(DeviceDataValidationService::class);
         $this->cacheService = $I->grabService(DeviceDataCacheService::class);
@@ -36,36 +38,55 @@ class DeviceDataResolverForSensorsCest
     /**
      * @param DeviceDataResolverStep $I
      * @param Example $example
-     * @example(type="temperature")
-     * @example(type="humidity")
-     * @example(type="pressure")
-     * @example(type="leakage")
-     * @example(type="dryContact")
+     * @example(type="temperature",payload="50")
+     * @example(type="humidity",payload="90")
+     * @example(type="pressure",payload="60")
+     * @example(type="leakage",payload="0")
+     * @example(type="dryContact",payload="0")
      * @return void
-     * @throws \Psr\Cache\InvalidArgumentException
+     * @throws InvalidArgumentException
      */
-    public function positiveResolveDevicePayloadTypeTemperature(DeviceDataResolverStep $I, Example $example): void
+    public function positiveResolveDevicePayload(DeviceDataResolverStep $I, Example $example): void
     {
-        $type = $example['type'];
         $payloadExample = $example['payload'];
-
+        $type = $example['type'];
         $sensor = $this->list[$type];
-        $event = Stub::makeEmpty(EventDispatcherInterface::class, ['dispatch' => Expected::never()]);
-        // payloadMin: "0" payloadMax: "100"
-        $payload = 50;
-        $dto = new DevicePayload($sensor->getTopic(), $payload);
-        $this->getResolver($event)->resolveDevicePayload($dto);
 
+        $event = Stub::makeEmpty(EventDispatcherInterface::class, ['dispatch' => Expected::never()]);
+
+        $dto = new DevicePayload($sensor->getTopic(), $payloadExample);
+        $this->getResolver($event)->resolveDevicePayload($dto);
         $cachedPayloadList = $this->cacheService->getPayloadByTopicList([$sensor->getTopic()]);
 
-        $I->assertEquals($payload, $cachedPayloadList[$sensor->getTopic()]);
+        $I->assertEquals($payloadExample, $cachedPayloadList[$sensor->getTopic()]);
     }
 
-    public function negativeResolveDevicePayloadTypeTemperature(DeviceDataResolverStep $I): void {}
-    public function negativeResolveDevicePayloadTypeHumidity(DeviceDataResolverStep $I): void {}
-    public function negativeResolveDevicePayloadTypePressure(DeviceDataResolverStep $I): void {}
-    public function negativeResolveDevicePayloadTypeLeakage(DeviceDataResolverStep $I): void {}
-    public function negativeResolveDevicePayloadTypeDryContact(DeviceDataResolverStep $I): void {}
+    /**
+     * @param DeviceDataResolverStep $I
+     * @param Example $example
+     * @example(type="temperature",payload="200")
+     * @example(type="humidity",payload="-20")
+     * @example(type="pressure",payload="999")
+     * @example(type="leakage",payload="1")
+     * @example(type="dryContact",payload="1")
+     * @return void
+     * @throws InvalidArgumentException
+     */
+    public function negativeResolveDevicePayload(DeviceDataResolverStep $I, Example $example): void {
+        $payloadExample = $example['payload'];
+        $type = $example['type'];
+        $sensor = $this->list[$type];
+
+        $event = Stub::makeEmpty(EventDispatcherInterface::class, [
+            'dispatch' => Expected::exactly(2, fn () => (object)[])
+        ]);
+
+        $dto = new DevicePayload($sensor->getTopic(), $payloadExample);
+        $this->getResolver($event)->resolveDevicePayload($dto);
+        $cachedPayloadList = $this->cacheService->getPayloadByTopicList([$sensor->getTopic()]);
+
+        $I->assertEquals($payloadExample, $cachedPayloadList[$sensor->getTopic()]);
+    }
 
     private function getResolver(EventDispatcherInterface $event): DeviceDataResolver
     {
