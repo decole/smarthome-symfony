9a159879dc9f5070eeaadedd0f1a2ea744e8aeee
decole <decole2014@yandex.ru>
2023-05-05 10:31:00 +0300

    fix CI/CD

--- tests/codeception_bootstrap.php
+++ tests/codeception_bootstrap.php
@@ -3,13 +3,18 @@
 use Symfony\Component\Dotenv\Dotenv;
 use Symfony\Component\ErrorHandler\Debug;
 
-require dirname(__DIR__).'/vendor/autoload.php';
+//require dirname(__DIR__).'/vendor/autoload.php';
 
-if (file_exists(dirname(__DIR__).'/config/bootstrap.php')) {
-    require dirname(__DIR__).'/config/bootstrap.php';
-} elseif (method_exists(Dotenv::class, 'bootEnv')) {
-    (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
-}
+//if (file_exists(dirname(__DIR__).'/config/bootstrap.php')) {
+//    require dirname(__DIR__).'/config/bootstrap.php';
+//} elseif (method_exists(Dotenv::class, 'bootEnv')) {
+//    (new Dotenv())->loadEnv(dirname(__DIR__) . '/.env', '.', 'test');
+//}
+
+$dotenv = (new Dotenv());
+$dotenv->usePutenv(true);
+$dotenv->loadEnv(dirname(__DIR__) . '/.env', '.', 'test');
+
 
 Debug::enable();
 
