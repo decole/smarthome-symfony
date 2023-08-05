33179a47a17f052410d0e58017df705189296613
decole <decole2014@yandex.ru>
2023-08-05 18:57:26 +0300

    [UBS-161] Оптимизация. CI-CD

--- ci_to_prod.sh
+++ ci_to_prod.sh
@@ -2,9 +2,17 @@
 
 set -ex
 
-# Prepare instance
+# prepare to release
+docker-compose pull
+docker-compose stop mqtt-bridge php-fpm
 
+# prepare instance
 docker-compose run -T php-fpm composer install --no-dev --optimize-autoloader
 docker-compose run -T commands php bin/console d:m:m --no-interaction
 docker-compose run -T commands chmod 777 -R ./var
 docker-compose run -T commands php bin/console cache:warmup
+
+# restart prod
+docker-compose down --remove-orphans
+docker-compose up -d
+docker-compose ps
